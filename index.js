b92a29a1acf0a473487e6f6750861213d2180e66
jarin3956 <jarinmeethal@gmail.com>
2024-01-29 00:43:42 +0530

    server root setup

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ connectDB();
 app.use('/', user_route);
 app.use('/admin', admin_route);
 
-const logTestData = async () => {
-    try {
-      const testDataDocuments = await TestData.find({});
-        console.log('Documents in testData collection:', testDataDocuments);
-    } catch (error) {
-      console.error('Error logging testData documents:', error);
-    } 
-  };
-  logTestData();
-
 app.listen(port, () => {
     console.log(`server running @ port ${port}`);
 })
